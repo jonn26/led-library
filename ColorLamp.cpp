39b5596a8e56f823691dd9e3576b04118ae3e19a
jonn26 <jonn26@gmail.com>
2014-04-08 15:28:36 -0700

    Update ColorLamp.cpp for 12bit rgb/hsb

--- ColorLamp.cpp
+++ ColorLamp.cpp
@@ -1,10 +1,10 @@
 /*
  * ColorLamp.h
- * 
- * Copyright (c) 2011 
- 
+ *
+ * Copyright (c) 2011
+
  * Remco Magielse & Serge Offermans
- 
+
  * Intelligent Lighting Institute (ILI), TU/e.
  *
  * All rights reserved. LAST UPDATE: 13-08-2012
@@ -14,8 +14,8 @@
 
 /* CONSTRUCTORS */
 
-/** Empty ColorLamp Constructor. If you use this, make sure to set the channel manually afterwards using setChannel(); 
-	this automatically assumes you use autoWrite (write to the arduino pins immediatily) and will set the pinMode for this channel to OUTPUT. 
+/** Empty ColorLamp Constructor. If you use this, make sure to set the channel manually afterwards using setChannel();
+	this automatically assumes you use autoWrite (write to the arduino pins immediatily) and will set the pinMode for this channel to OUTPUT.
 	If you use another method (e.g. DMX), use the full constructor and set autoWrite to false **/
  ColorLamp::ColorLamp( )
 {
@@ -38,14 +38,14 @@
   saturationAnim  = new Animation();
 }
 
-/** Colorlamp Constructor. The first three parameters set channels for Red, Green and Blue. 
+/** Colorlamp Constructor. The first three parameters set channels for Red, Green and Blue.
 	If autowrite is set to true; the arduino will write the output values to the arduino pins.
 	Set autoWrite to false if you do not connect your LEDs to arduino pins directly
     but use other channels (e.g. DMX, shift registers, TLC5940, etc.
 	If you have LEDs with a common anode (common +), you set that to true in order to invert the output values.
  **/
 
-ColorLamp::ColorLamp( uint16_t channelRed, uint16_t channelGreen, uint16_t channelBlue, bool autoWrite, bool commonAnode, uint16_t x, uint16_t y )
+ColorLamp::ColorLamp( uint32_t channelRed, uint32_t channelGreen, uint32_t channelBlue, bool autoWrite, bool commonAnode, uint32_t x, uint32_t y )
 {
   _x          	=  	x;
   _y          	=  	y;
@@ -64,7 +64,7 @@ ColorLamp::ColorLamp( uint16_t channelRed, uint16_t channelGreen, uint16_t chann
   _commonAnode 	= 	commonAnode;
   hueAnim  		=  	new Animation();
   saturationAnim  = new Animation();
-  
+
   if ( _autoWrite )
   {
 	pinMode( _channelRed, OUTPUT );
@@ -85,7 +85,7 @@ ColorLamp::~ColorLamp()
 /** Allows you to set the channel of the LED. if autoWrite is on, this is the Arduino pin that is actuated
 	If only one argument is supplied, the green and blue channel are the subsequent channel numbers
 **/
-void ColorLamp::setChannel( uint16_t channelRed, uint16_t channelGreen, uint16_t channelBlue )
+void ColorLamp::setChannel( uint32_t channelRed, uint32_t channelGreen, uint32_t channelBlue )
 {
 	if (channelGreen == 0 && channelBlue == 0) // This is the case if the user does not supply arguments for green and blue
 	{
@@ -105,19 +105,19 @@ void ColorLamp::setChannel( uint16_t channelRed, uint16_t channelGreen, uint16_t
 	}
 }
 
-/** This function immidatily sets the RED, GREEN and BLUE to the specified values. 
+/** This function immidatily sets the RED, GREEN and BLUE to the specified values.
 	Values are changed immediatily, but only actuated in the update() function.
 	When setting RGB; the values are first converted to HSB,
 	Conversion back to RGB happens during the update() function.
 	Any ongoing animations are stopped unless 'stopanimation' is set to false.
 **/
-void ColorLamp::setRGB( int r, int g, int b, bool stopAnimation )
+void ColorLamp::setRGB( uint32_t r, uint32_t g, uint32_t b, bool stopAnimation )
 {
-	if ( _red != r || _green != g || _blue != b ) 
-	{	
-		uint8_t hsb[3] = { 0 , 0 , 0 };
-		uint8_t * hsbArray = rgbToHsb(r, g, b, hsb );
-		
+	if ( _red != r || _green != g || _blue != b )
+	{
+		uint32_t hsb[3] = { 0 , 0 , 0 };
+		uint32_t * hsbArray = rgbToHsb(r, g, b, hsb );
+
 		setHSB(hsbArray[0], hsbArray[1], hsbArray[2], stopAnimation);
 	}
 }
@@ -127,7 +127,7 @@ void ColorLamp::setRed( uint8_t r, bool stopAnimation )
 {
 	uint8_t hsb[3] = { 0 , 0 , 0 };
 	uint8_t * hsbArray = rgbToHsb(r, _green, _blue, hsb );
-	
+
 	setHSB(hsbArray[0], hsbArray[1], hsbArray[2], stopAnimation);
 }
 
@@ -135,7 +135,7 @@ void ColorLamp::setGreen( uint8_t g, bool stopAnimation )
 {
 	uint8_t hsb[3] = { 0 , 0 , 0 };
 	uint8_t * hsbArray = rgbToHsb(_red, g, _blue, hsb );
-	
+
 	setHSB(hsbArray[0], hsbArray[1], hsbArray[2], stopAnimation);
 }
 
@@ -143,21 +143,21 @@ void ColorLamp::setBlue( uint8_t b, bool stopAnimation )
 {
 	uint8_t hsb[3] = { 0 , 0 , 0 };
 	uint8_t * hsbArray = rgbToHsb(_red, _green, b, hsb );
-	
+
 	setHSB(hsbArray[0], hsbArray[1], hsbArray[2], stopAnimation);
 } */
 
 /** This function immidatily sets the hue, saturation and intensity to the specified values.
-	Values are changed immediatily, but only actuated in the update() function. 
+	Values are changed immediatily, but only actuated in the update() function.
 	Any ongoing animations are stopped unless 'stopAnimation' is set to false.
 **/
-void ColorLamp::setHSB( int h, int s, int b, bool stopAnimation )
+void ColorLamp::setHSB( uint32_t h, uint32_t s, uint32_t b, bool stopAnimation )
 {
-	if ( _hue != h || _saturation != s || _intensity != b ) 
+	if ( _hue != h || _saturation != s || _intensity != b )
 	{
-		_hue 		= constrain( h, 0, 255 );
-		_saturation = constrain( s, 0, 255 );
-		_intensity 	= constrain( b, 0, 255 );
+		_hue 		= constrain( h, 0, 4095 );
+		_saturation = constrain( s, 0, 4095 );
+		_intensity 	= constrain( b, 0, 4095 );
 		_hasNewValue	= true;
 		if (stopAnimation)
 		{
@@ -172,11 +172,11 @@ void ColorLamp::setHSB( int h, int s, int b, bool stopAnimation )
 	Values are changed immediatily, but only actuated in the update() function.
 	maintains the other current variables. Any ongoing hue animation is stopped unless 'stopAnimation' is set to false.
 **/
-void ColorLamp::setHue( int h, bool stopAnimation )
+void ColorLamp::setHue( uint32_t h, bool stopAnimation )
 {
 	if( h != _hue )
 	{
-		_hue 			= constrain( h, 0, 255 );
+		_hue 			= constrain( h, 0, 4095 );
 		_hasNewValue	= true;
 		if (stopAnimation)
 		{
@@ -189,29 +189,29 @@ void ColorLamp::setHue( int h, bool stopAnimation )
 	maintains the other current variables. Any ongoing saturation animation is stopped unless 'stopAnimation' is set to false.
 	Values are changed immediatily, but only actuated in the update() function.
 **/
-void ColorLamp::setSaturation( int s, bool stopAnimation )
+void ColorLamp::setSaturation( uint32_t s, bool stopAnimation )
 {
 	if( s != _saturation )
 	{
-		_saturation = constrain( s, 0, 255 );
+		_saturation = constrain( s, 0, 4095 );
 		_hasNewValue	= true;
 		if (stopAnimation)
 		{
 			saturationAnim->stopAnimation();
 		}
 	}
-	
+
 }
 
 /** This function allows an animation in RGB terms.
 	When animating RGB; the values are first converted to HSB
 	Conversion back to RGB happens during the update function.
 **/
-void ColorLamp::rgbTo( int rTo, int gTo, int bTo, uint32_t duration )
+void ColorLamp::rgbTo( uint32_t rTo, uint32_t gTo, uint32_t bTo, uint32_t duration )
 {
-	uint8_t hsb[3] = { 0 , 0 , 0 };
-	uint8_t * hsbArray = rgbToHsb(rTo, gTo, bTo, hsb );
-	
+	uint32_t hsb[3] = { 0 , 0 , 0 };
+	uint32_t * hsbArray = rgbToHsb(rTo, gTo, bTo, hsb );
+
 	hsbTo(hsbArray[0], hsbArray[1], hsbArray[2], duration);
 }
 
@@ -220,7 +220,7 @@ void ColorLamp::redTo( uint8_t rTo, uint32_t duration )
 {
 	uint8_t hsb[3] = { 0 , 0 , 0 };
 	uint8_t * hsbArray = rgbToHsb(rTo, _green, _blue, hsb );
-	
+
 	hsbTo(hsbArray[0], hsbArray[1], hsbArray[2], duration);
 }
 
@@ -228,7 +228,7 @@ void ColorLamp::greenTo( uint8_t gTo, uint32_t duration )
 {
 	uint8_t hsb[3] = { 0 , 0 , 0 };
 	uint8_t * hsbArray = rgbToHsb(_red, gTo, _blue, hsb );
-	
+
 	hsbTo(hsbArray[0], hsbArray[1], hsbArray[2], duration);
 }
 
@@ -236,7 +236,7 @@ void ColorLamp::blueTo( uint8_t bTo, uint32_t duration )
 {
 	uint8_t hsb[3] = { 0 , 0 , 0 };
 	uint8_t * hsbArray = rgbToHsb(_red, _green, bTo, hsb );
-	
+
 	hsbTo(hsbArray[0], hsbArray[1], hsbArray[2], duration);
 } */
 
@@ -244,11 +244,11 @@ void ColorLamp::blueTo( uint8_t bTo, uint32_t duration )
 	shortcutThroughZero allows use of the hue color wheel as expected. If the shortest route from start value to end value goes through zero (red)
 	we will take this route by default. Set it to false if you wish to take the longer route.
 	**/
-void ColorLamp::hsbTo( int hTo, int sTo, int bTo, uint32_t duration, bool shortcutThroughZero )
-{	
-	hueAnim->startAnimation( _hue, constrain(hTo, 0, 255), duration, shortcutThroughZero );
-	saturationAnim->startAnimation( _saturation, constrain(sTo, 0, 255), duration );
-	intensityAnim->startAnimation( _intensity, constrain(bTo, 0, 255), duration );
+void ColorLamp::hsbTo( uint32_t hTo, uint32_t sTo, uint32_t bTo, uint32_t duration, bool shortcutThroughZero )
+{
+	hueAnim->startAnimation( _hue, constrain(hTo, 0, 4095), duration, shortcutThroughZero );
+	saturationAnim->startAnimation( _saturation, constrain(sTo, 0, 4095), duration );
+	intensityAnim->startAnimation( _intensity, constrain(bTo, 0, 4095), duration );
 }
 
 /** This function is used for animating to a desired HUE value while maintaining Saturation and Brightness values.
@@ -256,29 +256,29 @@ void ColorLamp::hsbTo( int hTo, int sTo, int bTo, uint32_t duration, bool shortc
 	shortcutThroughZero allows use of the hue color wheel as expected. If the shortest route from start value to end value goes through zero (red)
 	we will take this route by default. Set it to false if you wish to take the longer route.
 	**/
-void ColorLamp::hueTo( int hTo, uint32_t duration, bool shortcutThroughZero )
+void ColorLamp::hueTo( uint32_t hTo, uint32_t duration, bool shortcutThroughZero )
 {
-	hueAnim->startAnimation( _hue, constrain(hTo, 0, 255), duration, shortcutThroughZero );
+	hueAnim->startAnimation( _hue, constrain(hTo, 0, 4095), duration, shortcutThroughZero );
 }
 
 /** This function is used for animating to a desired Saturation value while maintaining Hue and Brightness values.
 	The duration of the animation can be set in millis().
 	**/
-void ColorLamp::saturationTo( int sTo, uint32_t duration )
+void ColorLamp::saturationTo( uint32_t sTo, uint32_t duration )
 {
-	saturationAnim->startAnimation( _saturation, constrain(sTo, 0, 255), duration );
+	saturationAnim->startAnimation( _saturation, constrain(sTo, 0, 4095), duration );
 }
 
 /** Tells the program you are using LEDs with a common anode (common +), if set to true (default = false).
-	This will invert the output values. If autoWrite == true, the arduino will make the ouput pins LOW in order to 
-	turn the LEDs on. If autoWrite == false, getRed(), getGreen() and getBlue() will return the inverted value (0 if it is full on). 
+	This will invert the output values. If autoWrite == true, the arduino will make the ouput pins LOW in order to
+	turn the LEDs on. If autoWrite == false, getRed(), getGreen() and getBlue() will return the inverted value (0 if it is full on).
  **/
 void ColorLamp::setCommonAnode( bool commonAnode )
 {
 	_commonAnode = commonAnode;
 }
 
-/** Sets the animation type for a colorLamp. The available animation types are LINEAR (no easing) and QUADRATIC. 
+/** Sets the animation type for a colorLamp. The available animation types are LINEAR (no easing) and QUADRATIC.
 	QUADRATIC animations allow independant easing in and out **/
 void ColorLamp::setAnimationType( uint8_t animType, bool easeIn, bool easeOut )
 {
@@ -311,34 +311,34 @@ void ColorLamp::update()
 	{
 		setIntensity( intensityAnim->getValue(), false );
 	}
-	
+
 	if ( _hasNewValue )
 	{
-		/*  Update the RGB values based on HSB to reflect the changes for actuation 
+		/*  Update the RGB values based on HSB to reflect the changes for actuation
 			This overwrites the current RGB values, even if they were recently set.
-			However, this is not a problem because if an RGB value is set; 
+			However, this is not a problem because if an RGB value is set;
 			it is also converted to matching HSB values that are thus up-to-date.
 		*/
-		uint8_t rgb[3];
-		uint8_t * rgbArray = hsbToRgb(_hue, _saturation, _intensity, rgb );
-		
-		_red 	= constrain( rgbArray[0], 0, 255 );
-		_green 	= constrain( rgbArray[1], 0, 255 );
-		_blue 	= constrain( rgbArray[2], 0, 255 );
-		
+		uint32_t rgb[3];
+		uint32_t * rgbArray = hsbToRgb(_hue, _saturation, _intensity, rgb );
+
+		_red 	= constrain( rgbArray[0], 0, 4095 );
+		_green 	= constrain( rgbArray[1], 0, 4095 );
+		_blue 	= constrain( rgbArray[2], 0, 4095 );
+
 		if( _autoWrite )
 		{
-			if ( !_commonAnode ) 
+			if ( !_commonAnode )
 			{
 				analogWrite(_channelRed, _red);
 				analogWrite(_channelGreen, _green);
 				analogWrite(_channelBlue, _blue);
 			}
-			else 
+			else
 			{
-				analogWrite(_channelRed, 255-_red);
-				analogWrite(_channelGreen, 255-_green);
-				analogWrite(_channelBlue, 255-_blue);
+				analogWrite(_channelRed, 4095-_red);
+				analogWrite(_channelGreen, 4095-_green);
+				analogWrite(_channelBlue, 4095-_blue);
 			}
 			_hasNewValue = false;
 		}
@@ -348,101 +348,101 @@ void ColorLamp::update()
 /* INTEGER FUNCTIONS */
 
 /** Returns the set channel for the RED LED **/
-uint16_t ColorLamp::getChannelRed()
+uint32_t ColorLamp::getChannelRed()
 {
 	return _channelRed;
 }
 /** Returns the set channel for the GREEN LED **/
-uint16_t ColorLamp::getChannelGreen()
+uint32_t ColorLamp::getChannelGreen()
 {
 	return _channelGreen;
 }
 /** Returns the set channel for the BLUE LED **/
-uint16_t ColorLamp::getChannelBlue()
+uint32_t ColorLamp::getChannelBlue()
 {
 	return _channelBlue;
 }
 
 /** This function returns the current RED output value. Be careful that they are inverted if commonAnode is set to true! **/
-uint8_t ColorLamp::getRed()
+uint32_t ColorLamp::getRed()
 {
 	if ( !_commonAnode )
-	{	
+	{
 		return _red;
 	}
-	else 
+	else
 	{
-		return 255-_red;
+		return 4095-_red;
 	}
 }
 
 /** This function returns the current GREEN output value. Be careful that they are inverted if commonAnode is set to true! **/
-uint8_t ColorLamp::getGreen()
+uint32_t ColorLamp::getGreen()
 {
 	if ( !_commonAnode )
-	{	
+	{
 		return _green;
 	}
-	else 
+	else
 	{
-		return 255-_green;
+		return 4095-_green;
 	}
 }
 
 /** This function returns the current GREEN output value. Be careful that they are inverted if commonAnode is set to true! **/
-uint8_t ColorLamp::getBlue()
+uint32_t ColorLamp::getBlue()
 {
 	if ( !_commonAnode )
-	{	
+	{
 		return _blue;
 	}
-	else 
+	else
 	{
-		return 255-_blue;
+		return 4095-_blue;
 	}
 }
 
 /** This function returns the current HUE output value. **/
-uint8_t ColorLamp::getHue()
+uint32_t ColorLamp::getHue()
 {
 	return _hue;
 }
 
 /** Returns the target value of the Hue animation
 **/
-uint8_t ColorLamp::getTargetHue()
+uint32_t ColorLamp::getTargetHue()
 {
 	return hueAnim->getEndValue();
 }
 
 /** This function returns the current SATURATION output value. **/
-uint8_t ColorLamp::getSaturation()
+uint32_t ColorLamp::getSaturation()
 {
 	return _saturation;
 }
 
 /** Returns the target value of the Saturation animation
 **/
-uint8_t ColorLamp::getTargetSaturation()
+uint32_t ColorLamp::getTargetSaturation()
 {
 	return saturationAnim->getEndValue();
 }
 
 /** This function returns the current INTENSITY output value. **/
-uint8_t ColorLamp::getBrightness()
+uint32_t ColorLamp::getBrightness()
 {
 	return _intensity;
 }
 
-/** This function converts RGB to HSB and is based on the one implemented 
+/** This function converts RGB to HSB and is based on the one implemented
 by robert Atkins in the RGB Converter Library
 https://github.com/ratkins/RGBConverter
 */
-uint8_t * ColorLamp::rgbToHsb(int r, int g, int b, uint8_t hsb[] ) 
+uint32_t * ColorLamp::rgbToHsb(uint32_t r, uint32_t g, uint32_t b, uint32_t hsb[] )
 {
-	float rd = (float) constrain(r,0,255)/255;
-    float gd = (float) constrain(g,0,255)/255;
-    float bd = (float) constrain(b,0,255)/255;
+	float rd = (float) r/4095;
+    float gd = (float) g/4095;
+    float bd = (float) b/4095;
     float maxVal = max(rd, max(gd, bd));
     float minVal = min(rd, min(gd, bd));
     float h = maxVal;
@@ -452,7 +452,7 @@ uint8_t * ColorLamp::rgbToHsb(int r, int g, int b, uint8_t hsb[] )
     float d = maxVal - minVal;
     s = maxVal == 0 ? 0 : d / maxVal;
 
-    if (maxVal == minVal) { 
+    if (maxVal == minVal) {
         h = 0; // achromatic
     } else {
         if (maxVal == rd) {
@@ -464,38 +464,32 @@ uint8_t * ColorLamp::rgbToHsb(int r, int g, int b, uint8_t hsb[] )
         }
         h /= 6;
     }
-    h *= 255;
-    s *= 255;
-    v *= 255;
+    h *= 4095;
+    s *= 4095;
+    v *= 4095;
 
-    hsb[0] = uint8_t(h);
-    hsb[1] = uint8_t(s);
-    hsb[2] = uint8_t(v);
+    hsb[0] = uint32_t(h);
+    hsb[1] = uint32_t(s);
+    hsb[2] = uint32_t(v);
 	return hsb;
 }
 
 
-/** This function converts RGB to HSB and is based on the one implemented 
+/** This function converts RGB to HSB and is based on the one implemented
 by Elco Jacobs in the ShiftPWM Library
 https://github.com/elcojacobs/ShiftPWM
 */
-uint8_t * ColorLamp::hsbToRgb( int h, int s, int v, uint8_t rgb[] ) 
-{	
-	uint8_t r = 0;
-	uint8_t g = 0;
-	uint8_t b = 0;
-	
-	h = constrain(h,0,255);
-	s = constrain(s,0,255);
-	v = constrain(v,0,255);
-	
-	uint16_t hue 	= map(h,0,255,0,359);
-	uint16_t phase 	= hue/60;
-	uint16_t bottom = ( (255 - s) * (float(v) / 255.0));
-	uint16_t top 	= v;
-	uint8_t rising  = ((top-bottom)  *(hue%60   )  )  /  60  +  bottom;
-	uint8_t falling = ((top-bottom)  *(60-hue%60)  )  /  60  +  bottom;
-	
+/*uint32_t * ColorLamp::hsbToRgb( uint32_t h, uint32_t s, uint32_t v, uint32_t rgb[] )
+{
+	uint32_t r,g,b;
+	//uint32_t hue 	= map(h,0,4095,0,359);
+	float hue 	= map(h,0,4095,0,359);
+	uint32_t phase 	= hue/60.0;
+	uint32_t bottom = ( (4095 - s) * (float(v) / 4095.0));
+	uint32_t top 	= v;
+	uint32_t rising  = ((top-bottom)  *(hue%60   )  )  /  60  +  bottom;
+	uint32_t falling = ((top-bottom)  *(60-hue%60)  )  /  60  +  bottom;
+
 	switch(phase) {
 	case 0:
 		r = top;
@@ -539,17 +533,49 @@ uint8_t * ColorLamp::hsbToRgb( int h, int s, int v, uint8_t rgb[] )
     rgb[2] = b;
 	return rgb;
 }
+*/
+
+uint32_t * ColorLamp::hsbToRgb( uint32_t h, uint32_t sat, uint32_t bri, uint32_t rgb[] )
+{
+  int32_t red_val;
+  int32_t green_val;
+  int32_t blue_val;
+  uint32_t hue = map(h,0,4095,0,12284);
+
+  if (hue < 4095) {
+    red_val = (5591040 - sat * (hue - 2730)) >> 12;
+    green_val = (5591040 - sat * (1365 - hue)) >> 12;
+    blue_val = (5591040 - sat * 1365) >> 12;
+  }
+  else if (hue < 8190) {
+    red_val = (5591040 - sat * 1365) >> 12;
+    green_val = (5591040 - sat * (hue - 6825)) >> 12;
+    blue_val = (5591040 - sat * (5460 - hue)) >> 12;
+  }
+  else {
+    red_val = (5591040 - sat * (9555 - hue)) >> 12;
+    green_val = (5591040 - sat * 1365) >> 12;
+    blue_val = (5591040 - sat * (hue - 10920)) >> 12;
+  }
+
+  rgb[0] = ((bri + 1) * red_val) >> 12;
+  rgb[1] = ((bri + 1) * green_val) >> 12;
+  rgb[2] = ((bri + 1) * blue_val) >> 12;
+
+	return rgb;
+}
+
 
 /* BOOL FUNCTIONS */
 
-/** This function returns true if any animation (either HUE, SATURATION, BRIGHTNESS) is ongoing in this ColorLamp **/ 
+/** This function returns true if any animation (either HUE, SATURATION, BRIGHTNESS) is ongoing in this ColorLamp **/
 bool ColorLamp::isAnimating()
 {
 	if ( !intensityAnim->isAnimating() && !hueAnim->isAnimating() && !saturationAnim->isAnimating() )
 	{
 		return false;
 	}
-	else 
+	else
 	{
 		return true;
 	}
@@ -568,9 +594,6 @@ bool ColorLamp::isAnimating(uint8_t param)
 		break;
 		case PARAM_SATURATION:
 			return saturationAnim->isAnimating();
-		break;
-		default:
-			return false;
 		break;
 	}
 }
